175db208bc82422adee2b7f22522bec28c1b04b0
mohammad <mkermani144@gmail.com>
2016-07-07 12:23:28 +0430

    Typewriting-stimulated placeholder for input field added.

--- desktop/js.js
+++ desktop/js.js
@@ -7,4 +7,10 @@ $(function(){
 		$('#add-btn').css('border-color','gray');
 		$(this).css('border-color','gray');
 	});
+	$('#add-input').typed({
+		strings: ['write encrypt method @2+5', 'read 5 pages of the book @1w+4'],
+		typeSpeed: 10,
+		backDelay: 60000,
+		loop: true
+	});
 });
