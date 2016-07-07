93a4696ac3db81e853e9bc0f14b14d093d4397bc
mkermani144 <mkermani144@gmail.com>
2016-07-07 15:59:39 +0430

    Frame removed.

--- desktop/main.js
+++ desktop/main.js
@@ -8,7 +8,7 @@ const {BrowserWindow} = electron;
 let win;
 
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({width: 800, height: 600, frame: false});
   win.loadURL(`file://${__dirname}/index.html`);
 	win.webContents.openDevTools();
 }
