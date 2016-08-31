13613b4cdfcf93b365699da2c9c636ccf116e62c
mkermani144 <mkermani144@gmail.com>
2016-08-31 19:43:41 +0430

    Choose color palette

---
+++ desktop/js/MainController.js
@@ -0,0 +1,7 @@
+angular.module('MainApp', ['ngMaterial'])
+    .config(($mdThemingProvider) => {
+        $mdThemingProvider.theme('default')
+            .primaryPalette('blue')
+            .accentPalette('pink')
+            .warnPalette('red');
+    });
--- desktop/main.js
+++ desktop/main.js
@@ -7,25 +7,28 @@ const fs = require('fs')
 var win
 
 function compileSass() {
-	sass.render({
-  file: 'css/main.scss',
-  outFile: 'css/main.css',
-  outputStyle: 'comporessed'
-	}, (err, result) => {
-    fs.writeFile('css/main.css', result.css, err => {})
-  })
+    sass.render({
+        file: 'css/main.scss',
+        outFile: 'css/main.css',
+        outputStyle: 'comporessed'
+    }, (err, result) => {
+        fs.writeFile('css/main.css', result.css, err => {})
+    })
 }
 
 function createWindow() {
-  win = new BrowserWindow({width: 800, height: 600})
-  win.loadURL(`file://${__dirname}/index.html`)
-	win.webContents.openDevTools()
-  win.on('close', () => {
-    fs.unlink(__dirname + '/css/main.css')
-  })
+    win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+    win.loadURL(`file://${__dirname}/index.html`)
+    win.webContents.openDevTools()
+    win.on('close', () => {
+        fs.unlink(__dirname + '/css/main.css')
+    })
 }
 
 app.on('ready', () => {
-  compileSass()
-  createWindow()
+    compileSass()
+    createWindow()
 })
