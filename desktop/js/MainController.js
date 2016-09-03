3f685d2a87f606940c389e8c983f948f22b71df8
mkermani144 <mkermani144@gmail.com>
2016-09-03 21:29:56 +0430

    Add very basic new-task popup

--- desktop/js/MainController.js
+++ desktop/js/MainController.js
@@ -4,4 +4,15 @@ angular.module('MainApp', ['ngMaterial'])
             .primaryPalette('blue')
             .accentPalette('pink')
             .warnPalette('red');
+    })
+    .controller('MainControl', ($mdDialog, $scope) => {
+      $scope.addNew = (ev) => {
+        $mdDialog.show(
+          $mdDialog.prompt()
+          .title('What do you wanna do?')
+          .targetEvent(ev)
+          .ok('Add')
+          .cancel('Cancel')
+        );
+      }
     });
