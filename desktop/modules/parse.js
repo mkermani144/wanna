c2d5d10990575489b3f28c0e7de2a343aa579e9f
mkermani144 <mkermani144@gmail.com>
2016-09-05 20:40:04 +0430

    Update parse module

---
+++ desktop/modules/parse.js
@@ -0,0 +1,24 @@
+/**
+ * Parse task and return task info if
+ * the task is valid, otherwise throw
+ * error.
+ * @param  {string} query  Enetered task
+ * @return {object}        Task info containing
+ *                         task text, start time
+ *                         and dealine
+ */
+function parse(query) {
+  var regex = /@(\d)$/
+  var regexResult = regex.exec(query)
+  if (!regexResult) {
+    throw "Invalid task query"
+  }
+  var text = query.slice(0, regexResult.index)
+  var start = Date.now()
+  var end = start + 86400 * regexResult[1]
+  return {
+    text,
+    start,
+    end
+  }
+}
