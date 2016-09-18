ca6f79cbe955720ce0b5476044d548a5d989e239
mohammad <mkermani144@gmail.com>
2016-09-18 16:17:09 +0430

    Add subheaders

--- desktop/app/components/tasks/directive.js
+++ desktop/app/components/tasks/directive.js
@@ -3,7 +3,7 @@ angular.module('MainApp')
     return (scope, el, attrs) => {
       scope.$watch('tasks', (newVal) => {
         console.log(typeof(el[0]), el[0]);
-        el[0].children[0].innerHTML = newVal[attrs.uaf];
+        el[0].children[0].children[0].innerHTML = newVal[attrs.uaf];
       }, true);
     }
   });
