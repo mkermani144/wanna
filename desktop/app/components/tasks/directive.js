8056260f1a05791a915b57eaad5561ee268d1107
mohammad <mkermani144@gmail.com>
2016-09-19 15:20:47 +0430

    Add left padding to tasks

--- desktop/app/components/tasks/directive.js
+++ desktop/app/components/tasks/directive.js
@@ -2,7 +2,6 @@ angular.module('MainApp')
   .directive('uaf', () => { // uaf = Update after find
     return (scope, el, attrs) => {
       scope.$watch('tasks', (newVal) => {
-        console.log(typeof(el[0]), el[0]);
         el[0].children[0].children[0].innerHTML = newVal[attrs.uaf];
       }, true);
     }
