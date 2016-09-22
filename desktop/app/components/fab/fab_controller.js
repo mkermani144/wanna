21031c47d2de54a72330e2e0d012e6a85050cbaf
mkermani144 <mkermani144@gmail.com>
2016-09-22 10:37:25 +0330

    Fix eslint errors - 2

--- desktop/app/components/fab/fab_controller.js
+++ desktop/app/components/fab/fab_controller.js
@@ -1,38 +1,39 @@
 angular.module('MainApp')
   .controller('FabControl', ($mdToast, $mdDialog, $scope, db) => {
-    function DialogController($scope, $mdDialog) {
-      $scope.cancel = function() {
+    const scope = $scope;
+    function DialogController() {
+      scope.cancel = function cancel() {
         $mdDialog.cancel();
       };
 
-      $scope.add = function(task) {
+      scope.add = function add(task) {
         if (task) {
           $mdDialog.hide(task);
         }
       };
     }
-    $scope.addNew = (ev) => {
+    scope.addNew = (ev) => {
       $mdDialog.show({
-          controller: DialogController,
-          templateUrl: 'app/components/fab/templates/newTaskDialog.html',
-          parent: angular.element(document.body),
-          targetEvent: ev,
-          clickOutsideToClose: true,
-          closeTo: {
-            top: 520,
-            left: 0,
-            width: 40,
-            height: 30
-          }
-        })
-        .then(task => {
-          db.insert(task);
-          $mdToast.show(
-            $mdToast.simple()
-            .textContent('Task added.')
-            .position('bottom start')
-          );
-        });
-    }
-
-  });
+        controller: DialogController,
+        templateUrl: 'app/components/fab/templates/newTaskDialog.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose: true,
+        closeTo: {
+          top: 520,
+          left: 0,
+          width: 40,
+          height: 30,
+        },
+      })
+      .then((task) => {
+        db.insert(task);
+        $mdToast.show(
+          $mdToast.simple()
+          .textContent('Task added.')
+          .position('bottom start')
+        );
+      });
+    };
+  }
+);
