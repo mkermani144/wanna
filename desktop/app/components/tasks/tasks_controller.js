0c04cb17c402f7ea64748a53e644bece2e6fa298
mkermani144 <mkermani144@gmail.com>
2016-09-23 21:08:44 +0330

    Add delete icon click slide animation

--- desktop/app/components/tasks/tasks_controller.js
+++ desktop/app/components/tasks/tasks_controller.js
@@ -1,6 +1,8 @@
 angular.module('MainApp')
   .controller('TaskControl', ($scope, db) => {
     $scope.isShown = true;
+    $scope.do = false;
+    $scope.delete = false;
     db.find('open', (tasks) => {
       $scope.tasks = tasks;
     });
