c951d161be8d2faf6e497d80e69998aa49a445bd
mkermani144 <mkermani144@gmail.com>
2016-09-29 11:32:00 +0330

    Remove "correct" word from test titles

--- desktop/tests/database.spec.js
+++ desktop/tests/database.spec.js
@@ -4,19 +4,19 @@ const parse = require('../app/shared/database/parse');
 describe('database', () => {
   describe('parse.js', () => {
     const testQuery = 'Learn something! @3+2w !';
-    it('should return correct task text', () => {
+    it('should return task text', () => {
       assert(parse(testQuery).text === 'Learn something!');
     });
-    it('should return correct starting point', () => {
+    it('should return starting point', () => {
       assert(parse(testQuery).start < Date.now() + (14 * 86400000) + 1000);
     });
-    it('should return correct ending point', () => {
+    it('should return ending point', () => {
       assert(parse(testQuery).end < Date.now() + (17 * 86400000) + 1000);
     });
-    it('should return correct importance', () => {
+    it('should return importance', () => {
       assert(parse(testQuery).importance === 2);
     });
-    it('should return correct status', () => {
+    it('should return status', () => {
       assert(parse(testQuery).status === 0);
     });
   });
