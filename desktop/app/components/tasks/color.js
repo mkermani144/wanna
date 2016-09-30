7030ea6bb49f3271370a6b9e7fbfeb915d258891
mkermani144 <mkermani144@gmail.com>
2016-09-30 17:03:28 +0330

    Add unit test for returnColorO of color.js

--- desktop/app/components/tasks/color.js
+++ desktop/app/components/tasks/color.js
@@ -20,7 +20,7 @@ function returnColorO(end, current) {
   const currentRounded = (current + (86400000 - (current % 86400000))) +
     (new Date().getTimezoneOffset() * 60000);
   const diff = (currentRounded - end) / 86400000;
-  return `hsla(0, 100%, ${50 / Math.pow(2, diff)}%, .3)`;
+  return `hsla(0, 100%, ${75 / Math.pow(2, diff - 1)}%, .3)`;
 }
 
 module.exports = {
--- desktop/tests/tasks.spec.js
+++ desktop/tests/tasks.spec.js
@@ -3,9 +3,17 @@ const color = require('../app/components/tasks/color');
 
 describe('tasks', () => {
   describe('color.js', () => {
-    const testTimes = [1475181000000, 1475181000000 + (2 * 86400000), 1475241362889];
-    it('should return green color hsla', () => {
-      assert(color.returnColor(...testTimes) === 'hsla(60, 100%, 75%, .3)');
+    describe('returnColor', () => {
+      const testTimes = [1475181000000, 1475353800000, 1475241362889];
+      it('should return green color hsla', () => {
+        assert(color.returnColor(...testTimes) === 'hsla(60, 100%, 75%, .3)');
+      });
+    });
+    describe('returnColorO', () => {
+      const testTimes = [1475181000000, 1475353800000, 1475353800001];
+      it('should return green color hsla', () => {
+        assert(color.returnColorO(...testTimes) === 'hsla(0, 100%, 37.5%, .3)');
+      });
     });
   });
 });
