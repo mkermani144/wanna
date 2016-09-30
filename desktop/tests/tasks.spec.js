f06cbc434aa2082ffeeadb4b35e3e02448ff366e
mkermani144 <mkermani144@gmail.com>
2016-09-30 18:45:00 +0330

    Update unit test messages

--- desktop/tests/tasks.spec.js
+++ desktop/tests/tasks.spec.js
@@ -5,13 +5,13 @@ describe('tasks', () => {
   describe('color.js', () => {
     describe('returnColor', () => {
       const testTimes = [1475181000000, 1475353800000, 1475241362889];
-      it('should return green color hsla', () => {
+      it('should return yellow color hsla', () => {
         assert(color.returnColor(...testTimes) === 'hsla(60, 100%, 75%, .3)');
       });
     });
     describe('returnColorO', () => {
       const testTimes = [1475181000000, 1475353800000, 1475353800001];
-      it('should return green color hsla', () => {
+      it('should return dark red color hsla', () => {
         assert(color.returnColorO(...testTimes) === 'hsla(0, 100%, 37.5%, .3)');
       });
     });
