c4316af250b0820a307f5babb671496ed605c86d
mkermani144 <mkermani144@gmail.com>
2016-09-30 16:05:00 +0330

    Update color.js to calculate based on rounded dates

--- desktop/app/components/tasks/color.js
+++ desktop/app/components/tasks/color.js
@@ -8,14 +8,18 @@
  * @return {string}          color
  */
 function returnColor(start, end, current) {
+  const currentRounded = (current + (86400000 - (current % 86400000))) +
+    (new Date().getTimezoneOffset() * 60000);
   const total = (end - start) / 86400000;
-  const part = (current - start) / 86400000;
+  const part = (currentRounded - start) / 86400000;
   const ratio = part / total;
   return `hsla(${(1 - ratio) * 200}, 100%, 75%, .3)`;
 }
 
 function returnColorO(end, current) {
-  const diff = (current - end) / 86400000;
+  const currentRounded = (current + (86400000 - (current % 86400000))) +
+    (new Date().getTimezoneOffset() * 60000);
+  const diff = (currentRounded - end) / 86400000;
   return `hsla(0, 100%, ${50 / Math.pow(2, diff)}%, .3)`;
 }
 
