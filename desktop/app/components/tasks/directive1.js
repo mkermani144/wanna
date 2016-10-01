8cea08683ee4f0d1698794c11e9938c6d2f9035e
mkermani144 <mkermani144@gmail.com>
2016-10-01 21:32:59 +0330

    Show units alongside task text

--- desktop/app/components/tasks/directive1.js
+++ desktop/app/components/tasks/directive1.js
@@ -9,7 +9,7 @@ angular.module('MainApp')
   .directive('uaf', () =>  // uaf = Update after find
     (scope, el, attrs) => {
       scope.$watch('openTasks', (newVal) => {
-        el[0].children[0].children[0].innerHTML = newVal[attrs.uaf].text;
+        el[0].children[0].children[0].innerHTML = `<p class="inline">${newVal[attrs.uaf].text}</p>&nbsp;<h6 class="inline">${newVal[attrs.uaf].units}</h6>`;
         angular.element(el[0]).css('backgroundColor', color.returnColor(newVal[attrs.uaf].start, newVal[attrs.uaf].end, Date.now()));
       });
     }
@@ -17,7 +17,7 @@ angular.module('MainApp')
   .directive('uafo', () =>  // uaf-o = Update after find, overdue
     (scope, el, attrs) => {
       scope.$watch('overdueTasks', (newVal) => {
-        el[0].children[0].children[0].innerHTML = newVal[attrs.uafo].text;
+        el[0].children[0].children[0].innerHTML = `<p class="inline">${newVal[attrs.uafo].text}</p>&nbsp;<h6 class="inline">${newVal[attrs.uafo].units}</h6>`;
         angular.element(el[0]).css('backgroundColor', color.returnColorO(newVal[attrs.uafo].end, Date.now()));
       });
     }
