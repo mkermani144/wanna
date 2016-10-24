42f1be2246056b972603b9202955be3b7c388572
mkermani144 <mkermani144@gmail.com>
2016-10-24 20:44:56 +0330

    Fix overdue directive bug

--- desktop/app/components/tasks/directive1.js
+++ desktop/app/components/tasks/directive1.js
@@ -35,14 +35,14 @@ angular.module('MainApp')
       scope.$watch('overdueTasks', (newVal) => {
         const icon = $compile('<md-icon md-svg-src="assets/img/cube.svg" class="small md-warn"></md-icon>')(scope);
         const isOneday = (newVal[attrs.uafo].end - newVal[attrs.uafo].start) === 86400000;
-        const isPeriodic = newVal[attrs.uaf].period !== -1;
+        const isPeriodic = newVal[attrs.uafo].period !== -1;
         let oneday;
         if (isOneday) {
           oneday = $compile('<md-icon md-svg-src="assets/img/one.svg" class="medium md-accent"></md-icon>')(scope);
         }
         let period;
         if (isPeriodic) {
-          period = newVal[attrs.uaf].period / 86400000;
+          period = newVal[attrs.uafo].period / 86400000;
           period = $compile(`<h6 class="inline grey">${period}day${period === 1 ? '' : 's'}</h6><md-icon md-svg-src="assets/img/repeat.svg" class="small"></md-icon>`)(scope);
         }
         el[0].children[0].children[0].innerHTML = `<p class="inline">${newVal[attrs.uafo].text}</p>&nbsp;<h6 class="inline mw">${newVal[attrs.uafo].units}</h6>`;
