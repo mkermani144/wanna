e25552cb699aae4b440a95ed4c504d03f61b4c7e
mohammad <mkermani144@gmail.com>
2016-11-21 13:33:20 +0330

    Add due tomorrow and due today warnings

--- desktop/app/components/tasks/directive1.js
+++ desktop/app/components/tasks/directive1.js
@@ -12,6 +12,9 @@ angular.module('MainApp')
         const icon = $compile('<md-icon md-svg-src="assets/img/cube.svg" class="small md-primary"></md-icon>')(scope);
         const isOneday = (newVal[attrs.uaf].end - newVal[attrs.uaf].start) === 86400000;
         const isPeriodic = newVal[attrs.uaf].period !== -1;
+        const now = Date.now();
+        const isDueToday = (newVal[attrs.uaf].end - now) < 86400000;
+        const isDueTomorrow = (newVal[attrs.uaf].end - now) < 86400000 * 2;
         let oneday;
         if (isOneday) {
           oneday = $compile('<md-icon md-svg-src="assets/img/one.svg" class="medium md-accent"></md-icon>')(scope);
@@ -21,10 +24,19 @@ angular.module('MainApp')
           period = newVal[attrs.uaf].period / 86400000;
           period = $compile(`<h6 class="inline grey">${period}day${period === 1 ? '' : 's'}</h6><md-icon md-svg-src="assets/img/repeat.svg" class="small"></md-icon>`)(scope);
         }
+        let dueToday = '';
+        let dueTomorrow = '';
+        if (isDueToday) {
+          dueToday = '<p class="inline">Due Today</p>';
+        } else if (isDueTomorrow) {
+          dueTomorrow = '<p class="inline">Due Tomorrow</p>';
+        }
         el[0].children[0].children[0].innerHTML = `<p class="inline">${newVal[attrs.uaf].text}</p>&nbsp;&nbsp;<h6 class="inline mp">${newVal[attrs.uaf].units}</h6>`;
         angular.element(el[0].children[0].children[0]).append(icon);
         angular.element(el[0].children[0].children[0]).append(oneday);
         angular.element(el[0].children[0].children[0]).append(period);
+        angular.element(el[0].children[0].children[0]).append(dueToday);
+        angular.element(el[0].children[0].children[0]).append(dueTomorrow);
         const borderLeft = `5px solid ${color.returnColor(newVal[attrs.uaf].start, newVal[attrs.uaf].end, Date.now())}`;
         angular.element(el[0]).css('borderLeft', borderLeft);
       });
