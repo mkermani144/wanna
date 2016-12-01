62e49b0aa6a02fb2ddf44fa472bbbb27b999d757
mkermani144 <mkermani144@gmail.com>
2016-12-01 19:30:42 +0330

    Resolve eslint error

--- desktop/app/shared/directives/directive.js
+++ desktop/app/shared/directives/directive.js
@@ -5,7 +5,7 @@
  */
 angular.module('MainApp')
   .directive('showonhover', () =>  // uafi = Update after find, ideas
-    (scope, el, attrs) => {
+    (scope, el) => {
       angular.element(el[0]).on('mouseenter', () => {
         const len = angular.element(el[0]).children().length;
         angular.element(angular.element(el[0]).children()[len - 1]).css('opacity', '1');
