37f979280dd2a4ce03eac1eb11c6a7d284121fe3
mkermani144 <mkermani144@gmail.com>
2016-12-17 19:18:48 +0330

    Add macbook image

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('#wanna img')[0].addEventListener('mouseover', () => {
+  this.src = 'l1.png';
+});
+document.querySelectorAll('#wanna img')[0].addEventListener('mouseout', () => {
+  this.src = 'lc.png';
+});
