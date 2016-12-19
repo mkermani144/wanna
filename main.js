f69769d24c90c362ba65f91e0a3aca79453062e3
mohammad <mkermani144@gmail.com>
2016-12-19 08:11:52 +0330

    Add entry for Mac OS X

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const oses = {
   'Linux x86_64': 'Linux',
   Win32: 'Windows',
-  // TODO: Add Os X entry
+  MacIntel: 'Mac OS X',
 };
 document.querySelectorAll('#wanna button')[0].innerHTML += ` for ${oses[navigator.platform]}`;
