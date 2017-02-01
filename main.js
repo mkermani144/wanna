d1bb015dfe01d7ab19c4a46b07fe586471d64f39
mohammad <mkermani144@gmail.com>
2017-02-01 17:53:18 +0330

    Resolve eslint error

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
+/* global $ */
 const oses = {
   'Linux x86_64': 'Linux',
   Win32: 'Windows',
   MacIntel: 'Mac OS X',
 };
 // document.querySelectorAll('#wanna button')[0].innerHTML += ` for ${oses[navigator.platform]}`;
-$('#wanna button').html($('#wanna button').html() + ` for ${oses[navigator.platform]}`);
+$('#wanna button').html(`{$('#wanna button').html()} for ${oses[navigator.platform]}`);
