d4e017a3a86e5517292ef2f8c2c68bf0eb4fa344
mkermani144 <mkermani144@gmail.com>
2017-02-02 16:41:49 +0330

    Fix minor bug

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 /* global $ */
-const oses = {
+/* global navigator */
+const os = {
   'Linux x86_64': 'Linux',
   Win32: 'Windows',
   MacIntel: 'Mac OS X',
 };
-// document.querySelectorAll('#wanna button')[0].innerHTML += ` for ${oses[navigator.platform]}`;
-$('#wanna button').html(`{$('#wanna button').html()} for ${oses[navigator.platform]}`);
+$('#wanna button').html(`Download for ${os[navigator.platform]}`);
