ff20215acdcf131d082a23d9368bd2664cd29008
mohammad <mkermani144@gmail.com>
2017-05-10 14:42:02 +0430

    Update gh-pages link to downlaod wanna

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 /* global navigator */
 const oses = {
   'Linux x86_64': 'Linux',
-  Win32: 'Windows',
-  MacIntel: 'Mac OS X',
+  // Win32: 'Windows',
+  // MacIntel: 'Mac OS X',
 };
 const currentOs = navigator.platform;
 if (currentOs in oses) {
-  $('#wrapper button').html(`Download for ${oses[currentOs]}`);
+  $('#wrapper #button').html(`Download for ${oses[currentOs]}`);
 } else {
-  $('#wrapper button').replaceWith('<p id="sorry">Sorry, Wanna is not supported on your platform :(</p>');
+  $('#wrapper #button').replaceWith('<p id="sorry">Sorry, Wanna is not supported on your platform (yet) :(</p>');
 }
