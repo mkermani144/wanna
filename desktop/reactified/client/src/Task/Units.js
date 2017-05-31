7d5eee560c1f6e64e1f56df785ead9d755bc23de
mkermani144 <mkermani144@gmail.com>
2017-05-31 18:51:43 +0430

    Update units color

--- desktop/reactified/client/src/Task/Units.js
+++ desktop/reactified/client/src/Task/Units.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import { blue700 } from 'material-ui/styles/colors';
+
 class Units extends Component {
   render() {
+    const style = {
+      color: blue700,
+    };
     return (
-      <small className="Units">{this.props.units} units</small>
+      <small className="Units" style={style}>{this.props.units} units</small>
     );
   }
 }
