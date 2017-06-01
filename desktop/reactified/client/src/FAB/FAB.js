ec27f07dfb832cdc9cf5ccf1fe51775252ed9594
mkermani144 <mkermani144@gmail.com>
2017-06-01 10:51:22 +0430

    Increase FAB z-index

--- desktop/reactified/client/src/FAB/FAB.js
+++ desktop/reactified/client/src/FAB/FAB.js
@@ -20,6 +20,7 @@ class FAB extends Component {
         position: 'absolute',
         right: '20px',
         bottom: '20px',
+        zIndex: 1000,
       },
       newTask: {
         color: green600,
