c531bd890961a4820bf06bf43dd7ea7272db5c95
mkermani144 <mkermani144@gmail.com>
2017-06-01 10:12:30 +0430

    Add Actions component

---
+++ desktop/reactified/client/src/Idea/Actions.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import IconButton from 'material-ui/IconButton';
+
+import Arrow from 'material-ui/svg-icons/navigation/arrow-forward';
+import Edit from 'material-ui/svg-icons/image/edit';
+import Delete from 'material-ui/svg-icons/action/delete';
+
+import { green500, grey600, red500 } from 'material-ui/styles/colors';
+
+class Actions extends Component {
+  render() {
+    const colors = {
+      arrow: green500,
+      edit: grey600,
+      delete: red500
+    };
+    return (
+      <div className="Actions">
+        <IconButton className="IconButton">
+          <Arrow color={colors.arrow} />
+        </IconButton>
+        <IconButton className="IconButton">
+          <Edit color={colors.edit} />
+        </IconButton>
+        <IconButton className="IconButton">
+          <Delete color={colors.delete} />
+        </IconButton>
+      </div>
+    );
+  }
+}
+
+export default Actions;
