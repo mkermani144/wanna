ef40e3780de80ac723ef3ebce4c93c6880a7938c
mkermani144 <mkermani144@gmail.com>
2017-06-02 22:05:05 +0430

    Add task actionCreators

---
+++ desktop/reactified/client/src/Task/actionsCreators.js
@@ -0,0 +1,15 @@
+const doTask = taskId => ({
+  type: 'CONVERT_IDEA',
+  taskId,
+});
+const editTask = (taskId, newTask) => ({
+  type: 'EDIT_IDEA',
+  taskId,
+  ...newTask
+});
+const deleteTask = taskId => ({
+  type: 'DELETE_IDEA',
+  taskId,
+});
+
+export { convertIdea, editIdea, deleteIdea };
