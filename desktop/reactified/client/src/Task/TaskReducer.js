2edf25f79f63da1aa4e211d73c31a17b8a6ed1be
mkermani144 <mkermani144@gmail.com>
2017-06-04 14:18:12 +0430

    Add TaskReducer

---
+++ desktop/reactified/client/src/Task/TaskReducer.js
@@ -0,0 +1,28 @@
+const TaskRedcuer = (state, action) => {
+  switch (action.type) {
+    case 'DO_TASK':
+      return [
+        ...state.slice(0, action.index),
+        {
+          ...state[action.index],
+          done: true,
+        },
+        ...state.slice(action.index + 1),
+      ];
+    case 'EDIT_TASK':
+      return [
+        ...state.slice(0, action.index),
+        action.newTask,
+        ...state.slice(action.index + 1),
+      ];
+    case 'DELETE_TASK':
+      return [
+        ...state.slice(0, action.index),
+        ...state.slice(action.index + 1),
+      ];
+    default:
+      return state;
+  }
+};
+
+export default TaskRedcuer;
