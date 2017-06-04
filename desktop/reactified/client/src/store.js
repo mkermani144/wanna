df7386111b95be9f749dbd17c50dfaab2ecd4246
mkermani144 <mkermani144@gmail.com>
2017-06-04 12:16:47 +0430

    Update store default state

--- desktop/reactified/client/src/store.js
+++ desktop/reactified/client/src/store.js
@@ -2,7 +2,9 @@ import { createStore } from 'redux';
 import rootReducer from './reducer';
 
 const defaultState = {
-
+  tasks: [],
+  ideas: [],
+  showNotYetTasks: true,
 };
 
 const store = createStore(rootReducer, defaultState);
