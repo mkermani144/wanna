147ec5f5740af79f9e0d6e7e7a766a9cb047602a
mkermani144 <mkermani144@gmail.com>
2017-06-07 19:39:03 +0430

    Remove IdeaContainer

--- desktop/reactified/client/src/Idea/IdeaListContainer.js
+++ desktop/reactified/client/src/Idea/IdeaListContainer.js
@@ -1,9 +1,14 @@
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import * as actionCreators from './actionCreators';
 
 import IdeaList from './IdeaList';
 
 const mapStateToProps = state => ({ ideas: state.ideas });
 
-const IdeaListContainer = connect(mapStateToProps, null)(IdeaList);
+const mapDispatchToProps = dispatch => bindActionCreators(actionCreators, dispatch);
+
+const IdeaListContainer = connect(mapStateToProps, mapDispatchToProps)(IdeaList);
 
 export default IdeaListContainer;
