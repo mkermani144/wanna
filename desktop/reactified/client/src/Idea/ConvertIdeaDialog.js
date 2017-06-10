83cb40dc6c867a8f68d39886b53be196d94edf0d
mkermani144 <mkermani144@gmail.com>
2017-06-10 22:22:38 +0430

    Add ConvertIdeaDialog component

---
+++ desktop/reactified/client/src/Idea/ConvertIdeaDialog.js
@@ -0,0 +1,216 @@
+import React, { Component } from 'react';
+
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import TextField from 'material-ui/TextField';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+import './ConvertIdeaDialog.css';
+
+class NewTaskDialog extends Component {
+  constructor() {
+    super();
+    this.state = {
+      periodValue: 1,
+      startValue: 1,
+      estimationValue: 1,
+      repetitionValue: 1,
+      task: '',
+      period: '',
+      start: '',
+      estimation: '',
+      repetition: '',
+    };
+  }
+  handlePeriodMenuChange = (e, i, value) => {
+    this.setState({
+      periodValue: value,
+    });
+  }
+  handleStartMenuChange = (e, i, value) => {
+    this.setState({
+      startValue: value,
+    });
+  }
+  handleEstimationMenuChange = (e, i, value) => {
+    this.setState({
+      estimationValue: value,
+    });
+  }
+  handleRepetitionMenuChange = (e, i, value) => {
+    this.setState({
+      repetitionValue: value,
+    });
+  }
+  handleTaskChange = (e) => {
+    this.setState({
+      task: e.target.value,
+    });
+  }
+  handlePeriodChange = (e) => {
+    this.setState({
+      period: e.target.value,
+    });
+  }
+  handleStartChange = (e) => {
+    this.setState({
+      start: e.target.value,
+    });
+  }
+  handleEstimationChange = (e) => {
+    this.setState({
+      estimation: e.target.value,
+    });
+  }
+  handleRepetitionChange = (e) => {
+    this.setState({
+      repetition: e.target.value,
+    });
+  }
+  handleRequestClose = () => {
+    this.setState({
+      periodValue: 1,
+      startValue: 1,
+      estimationValue: 1,
+      repetitionValue: 1,
+      task: '',
+      period: '',
+      start: '',
+      estimation: '',
+      repetition: '',
+    });
+    this.props.onRequestClose();
+  }
+  handleRequestConvert = () => {
+    this.props.onRequestConvert(this.state);
+    this.setState({
+      periodValue: 1,
+      startValue: 1,
+      estimationValue: 1,
+      repetitionValue: 1,
+      task: '',
+      period: '',
+      start: '',
+      estimation: '',
+      repetition: '',
+    });
+  }
+  handleRequestFinish = () => {
+    this.props.onRequestConvert(this.state);
+    this.props.onRequestDelete();
+    this.props.onRequestClose();
+    this.setState({
+      periodValue: 1,
+      startValue: 1,
+      estimationValue: 1,
+      repetitionValue: 1,
+      task: '',
+      period: '',
+      start: '',
+      estimation: '',
+      repetition: '',
+    });
+  }
+  render() {
+    const actions = [
+      <FlatButton
+        label="Add and finish"
+        primary={true}
+        disabled={!Boolean(this.state.task && this.state.period && this.state.estimation)}
+        onTouchTap={this.handleRequestFinish}
+      />,
+      <FlatButton
+        label="Add and continue"
+        primary={true}
+        disabled={!Boolean(this.state.task && this.state.period && this.state.estimation)}
+        onTouchTap={this.handleRequestConvert}
+      />,
+      <FlatButton
+        label="Cancel"
+        primary={true}
+        onTouchTap={this.handleRequestClose}
+      />,
+    ];
+    return (
+      <div className="NewTaskDialog">
+        <Dialog
+          title="Convert idea"
+          actions={actions}
+          open={this.props.open}
+          onRequestClose={this.props.onRequestClose}
+        >
+          Converting idea: {this.props.idea}
+          <br />
+          <div className="textfields">
+            <TextField
+              floatingLabelText="Task title"
+              fullWidth={true}
+              onChange={this.handleTaskChange}
+              autoFocus
+              value={this.state.task}
+            />
+            <div className="row">
+              <TextField
+                floatingLabelText="Period         "
+                onChange={this.handlePeriodChange}
+                value={this.state.period}
+              />
+              <DropDownMenu
+                value={this.state.periodValue}
+                onChange={this.handlePeriodMenuChange}
+              >
+                <MenuItem value={1} primaryText="Days" />
+                <MenuItem value={7} primaryText="Weeks" />
+              </DropDownMenu>
+            </div>
+            <div className="row">
+              <TextField
+                floatingLabelText="Time to start"
+                onChange={this.handleStartChange}
+                value={this.state.start}
+              />
+              <DropDownMenu
+                value={this.state.startValue}
+                onChange={this.handleStartMenuChange}
+              >
+                <MenuItem value={1} primaryText="Days" />
+                <MenuItem value={7} primaryText="Weeks" />
+              </DropDownMenu>
+            </div>
+            <div className="row">
+              <TextField
+                floatingLabelText="Estimated time"
+                onChange={this.handleEstimationChange}
+                value={this.state.estimation}
+              />
+              <DropDownMenu
+                value={this.state.estimationValue}
+                onChange={this.handleEstimationMenuChange}
+              >
+                <MenuItem value={1} primaryText="Minutes" />
+                <MenuItem value={60} primaryText="Hours" />
+              </DropDownMenu>
+            </div>
+            <div className="row">
+              <TextField
+                floatingLabelText="Repetition period"
+                onChange={this.handleRepetitionChange}
+                value={this.state.repetition}
+              />
+              <DropDownMenu
+                value={this.state.repetitionValue}
+                onChange={this.handleRepetitionMenuChange}
+              >
+                <MenuItem value={1} primaryText="Days" />
+                <MenuItem value={7} primaryText="Weeks" />
+              </DropDownMenu>
+            </div>
+          </div>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default NewTaskDialog;
