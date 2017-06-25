f37638c8f5e3e397bb0868bd971621383491db2a
mkermani144 <mkermani144@gmail.com>
2017-06-25 15:02:22 +0430

    Fix eslint errors of App.js

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import { Provider } from 'react-redux';
-import store from './store';
-
 import { Redirect, Route, BrowserRouter as Router } from 'react-router-dom';
-
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-
 import { blue500 } from 'material-ui/styles/colors';
 
+import store from './store';
 import Sidebar from './Sidebar/Sidebar';
 import FABContainer from './FAB/FABContainer';
 import TaskListContainer from './Task/TaskListContainer';
@@ -24,34 +20,32 @@ import './App.css';
 
 injectTapEventPlugin();
 
-class App extends Component {
-  render() {
-    const muiTheme = getMuiTheme({
-      palette: {
-        primary1Color: blue500,
-      },
-    });
-    store.subscribe(() => {
-      update(store.getState());
-    });
-    return (
-      <Provider store={store}>
-        <Router>
-          <MuiThemeProvider muiTheme={muiTheme}>
-            <div className="App">
-              <Sidebar />
-              <FABContainer />
-              <Redirect from="/" to="tasks" />
-              <Route path="/tasks" component={TaskListContainer} />
-              <Route path="/ideas" component={IdeaListContainer} />
-              <Route path="/settings" component={SettingsContainer} />
-              <Route path="/about" component={About} />
-            </div>
-          </MuiThemeProvider>
-        </Router>
-      </Provider>
-    );
-  }
-}
+const App = () => {
+  const muiTheme = getMuiTheme({
+    palette: {
+      primary1Color: blue500,
+    },
+  });
+  store.subscribe(() => {
+    update(store.getState());
+  });
+  return (
+    <Provider store={store}>
+      <Router>
+        <MuiThemeProvider muiTheme={muiTheme}>
+          <div className="App">
+            <Sidebar />
+            <FABContainer />
+            <Redirect from="/" to="tasks" />
+            <Route path="/tasks" component={TaskListContainer} />
+            <Route path="/ideas" component={IdeaListContainer} />
+            <Route path="/settings" component={SettingsContainer} />
+            <Route path="/about" component={About} />
+          </div>
+        </MuiThemeProvider>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
