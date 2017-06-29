59bfb8585a35e06128dd5fd5e6181befc7b2ed7c
mkermani144 <mkermani144@gmail.com>
2017-06-29 11:14:28 +0430

    Fix #184

--- desktop/main.js
+++ desktop/main.js
@@ -4,17 +4,40 @@ const electron = require('electron');
 const { app } = electron;
 const { BrowserWindow } = electron;
 
+const dbExists = () => {
+  const dbPath = '.config/db';
+  if (fs.existsSync(dbPath)) {
+    return true;
+  }
+  return false;
+};
+
+const createDatabase = () => {
+  fs.mkdirSync('./.config');
+  const prefill = JSON.stringify({
+    tasks: [],
+    ideas: [],
+    appProperties: {
+      showNotYetTasks: true,
+      fullscreen: true,
+      calendarSystem: 'en-US',
+      firstDayOfWeek: 1,
+    },
+  });
+  fs.writeFileSync('.config/db', prefill);
+};
+
 const isFullscreen = () => {
   const data = fs.readFileSync('.config/db', 'utf-8');
-  if (JSON.parse(data).appProperties) {
-    return JSON.parse(data).appProperties.fullscreen;
-  }
-  return true;
+  return JSON.parse(data).appProperties.fullscreen;
 };
 
 let win;
 
 function createWindow() {
+  if (dbExists() === false) {
+    createDatabase();
+  }
   win = new BrowserWindow({
     minWidth: 800,
     minHeight: 600,
