aaaf820227dc67d685e88220f1235b6ad5104d25
mkermani144 <mkermani144@gmail.com>
2017-07-04 20:10:02 +0430

    Fix #156

--- desktop/src/Sidebar/Sidebar.js
+++ desktop/src/Sidebar/Sidebar.js
@@ -8,9 +8,13 @@ import Settings from 'material-ui/svg-icons/action/settings';
 import InfoOutline from 'material-ui/svg-icons/action/info-outline';
 import {
   green600,
+  green800,
   yellow800,
-  blueGrey600,
-  grey600,
+  yellow900,
+  indigo600,
+  indigo900,
+  cyan600,
+  cyan700,
 } from 'material-ui/styles/colors';
 
 import './Sidebar.css';
@@ -20,9 +24,29 @@ class Sidebar extends PureComponent {
     super();
     this.state = {
       expanded: false,
+      current: 'tasks',
     };
   }
+  changeActiveTab = (tab) => {
+    this.setState({
+      current: tab,
+    });
+  }
   render() {
+    const styles = {
+      tasks: {
+        color: green800,
+      },
+      ideas: {
+        color: yellow900,
+      },
+      settings: {
+        color: indigo900,
+      },
+      about: {
+        color: cyan700,
+      },
+    };
     return (
       <Drawer
         className="Sidebar"
@@ -33,23 +57,79 @@ class Sidebar extends PureComponent {
         <List>
           <ListItem
             primaryText="Tasks"
-            leftIcon={<DoneAll color={green600} />}
+            leftIcon={
+              <DoneAll
+                color={
+                  this.state.current === 'tasks' ?
+                  green600 :
+                  null
+                }
+              />
+            }
+            style={
+              this.state.current === 'tasks' ?
+              styles.tasks :
+              null
+            }
             containerElement={<Link to="/tasks" />}
+            onClick={() => this.changeActiveTab('tasks')}
           />
           <ListItem
             primaryText="Ideas"
-            leftIcon={<LightbulbOutline color={yellow800} />}
+            leftIcon={
+              <LightbulbOutline
+                color={
+                  this.state.current === 'ideas' ?
+                  yellow800 :
+                  null
+                }
+              />
+            }
+            style={
+              this.state.current === 'ideas' ?
+              styles.ideas :
+              null
+            }
             containerElement={<Link to="/ideas" />}
+            onClick={() => this.changeActiveTab('ideas')}
           />
           <ListItem
             primaryText="Settings"
-            leftIcon={<Settings color={blueGrey600} />}
+            leftIcon={
+              <Settings
+                color={
+                  this.state.current === 'settings' ?
+                  indigo600 :
+                  null
+                }
+              />
+            }
+            style={
+              this.state.current === 'settings' ?
+              styles.settings :
+              null
+            }
             containerElement={<Link to="/settings" />}
+            onClick={() => this.changeActiveTab('settings')}
           />
           <ListItem
             primaryText="About"
-            leftIcon={<InfoOutline color={grey600} />}
+            leftIcon={
+              <InfoOutline
+                color={
+                  this.state.current === 'about' ?
+                  cyan600 :
+                  null
+                }
+              />
+            }
+            style={
+              this.state.current === 'about' ?
+              styles.about :
+              null
+            }
             containerElement={<Link to="/about" />}
+            onClick={() => this.changeActiveTab('about')}
           />
         </List>
       </Drawer>
