104e0c435c7f6a52643a743cadb4fc205e337366
mkermani144 <mkermani144@gmail.com>
2017-07-09 15:21:47 +0430

    Fix #173

--- desktop/src/Idea/Actions.js
+++ desktop/src/Idea/Actions.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import IconButton from 'material-ui/IconButton';
-import Arrow from 'material-ui/svg-icons/navigation/arrow-forward';
 import Edit from 'material-ui/svg-icons/image/edit';
 import Delete from 'material-ui/svg-icons/action/delete';
 import { green500, grey600, red500 } from 'material-ui/styles/colors';
+
+import Fork from '../icons/fork';
 
 const Actions = (props) => {
   const colors = {
@@ -17,7 +18,7 @@ const Actions = (props) => {
         className="IconButton"
         onTouchTap={props.onRequestConvertDialogOpen}
       >
-        <Arrow color={colors.arrow} />
+        <Fork color={colors.arrow} />
       </IconButton>
       <IconButton
         className="IconButton"
