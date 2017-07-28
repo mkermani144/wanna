02a1105d26fde74893e4289a86b47004a4a4b178
mohammad .k <mkermani144@gmail.com>
2017-07-28 19:04:45 +0430

    Update help tab hotkey

--- desktop/src/App.js
+++ desktop/src/App.js
@@ -38,7 +38,7 @@ class App extends PureComponent {
       showTasks: 'shift+t',
       showIdeas: 'shift+i',
       showSettings: 'shift+s',
-      showHelp: 'shift+a',
+      showHelp: 'shift+h',
     };
   }
   handleSidebarToggle = () => {
