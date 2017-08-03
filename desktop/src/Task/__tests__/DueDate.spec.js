b4ce0c4e2ea27dbb249c11825b222cc6d6ca3db5
mkermani144 <mkermani144@gmail.com>
2017-08-03 21:07:41 +0430

    Update Task DueDate unit test

--- desktop/src/Task/__tests__/DueDate.spec.js
+++ desktop/src/Task/__tests__/DueDate.spec.js
@@ -12,7 +12,11 @@ it('should be a <span />', () => {
   const dueDate = shallow(<DueDate />);
   expect(dueDate.is('span.DueDate')).toEqual(true);
 });
-it('should show icon if due date is close', () => {
+it('should show icon if due date is tomorrow', () => {
+  const dueDate = shallow(<DueDate due="tomorrow" />);
+  expect(dueDate.find('small').length).toBeGreaterThan(0);
+});
+it('should show icon if due date is today', () => {
   const dueDate = shallow(<DueDate due="today" />);
   expect(dueDate.find('small').length).toBeGreaterThan(0);
 });
