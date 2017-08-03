97e5267de08664488644337eb163e5d8f55e209d
mkermani144 <mkermani144@gmail.com>
2017-08-03 20:24:33 +0430

    Add Idea Actions unit test

---
+++ desktop/src/Idea/__tests__/Actions.spec.js
@@ -0,0 +1,14 @@
+/* eslint-env mocha, jest */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Actions from '../Actions';
+
+it('should render', () => {
+  shallow(<Actions />);
+});
+it('should have 3 <IconButton />', () => {
+  const actions = shallow(<Actions />);
+  expect(actions.find('IconButton').length).toEqual(3);
+});
