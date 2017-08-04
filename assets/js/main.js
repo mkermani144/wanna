d7a3825bd87a4da65bef26277b426393ae40a36f
mkermani144 <mkermani144@gmail.com>
2017-08-04 17:51:57 +0430

    Fix download button link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
 const OSes = {
-  'Linux x86_64': ['Linux', 'linux'],
-  Win32: ['Windows', 'windows'],
-  MacIntel: ['OSX', 'apple'],
+  'Linux x86_64': ['Linux (.deb)', 'linux', '.deb'],
+  Win32: ['Windows', 'windows', '.exe'],
+  // MacIntel: ['OSX', 'apple'],
 };
 const currentOS = navigator.platform;
 if (Object.keys(OSes).indexOf(currentOS) >= 0) {
-  $('.download').html(`Download for ${OSes[currentOS][0]} &nbsp; <i class="fa fa-lg fa-${OSes[currentOS][1]}"></i>`);
+  const $button = $('.download');
+  $button.html(`Download for ${OSes[currentOS][0]} &nbsp; <i class="fa fa-lg fa-${OSes[currentOS][1]}"></i>`);
+  $button.attr('href', 'https://github.com/mkermani144/wanna/releases/download/Flex-alpha/Wanna-setup-x64' + OSes[currentOS][2]);
 }
