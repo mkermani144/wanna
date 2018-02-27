0bbc6b6fd42aa33302efdbbf8362b31861244633
mohammad <mkermani144@gmail.com>
2018-02-27 17:26:12 +0330

    Update github pages for flex alpha 2 release

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,13 @@
 const OSes = {
   'Linux x86_64': ['Linux (.deb)', 'linux', '.deb'],
   Win32: ['Windows', 'windows', '.exe'],
-  // MacIntel: ['OSX', 'apple'],
+  MacIntel: ['OSX', 'apple', '.dmg'],
 };
 const currentOS = navigator.platform;
 if (Object.keys(OSes).indexOf(currentOS) >= 0) {
   const $button = $('.download');
   $button.html(`Download for ${OSes[currentOS][0]} &nbsp; <i class="fa fa-lg fa-${OSes[currentOS][1]}"></i>`);
-  $button.attr('href', 'https://github.com/mkermani144/wanna/releases/download/Flex-alpha/Wanna-setup-x64' + OSes[currentOS][2]);
+  $button.attr('href', 'https://github.com/mkermani144/wanna/releases/download/1.0.0-alpha.2%2Bflex/wanna-1.0.0-alpha.2' + OSes[currentOS][2]);
 } else {
   const $button = $('.download');
   $button.html('Download for your platform - <i>soon<i>');
